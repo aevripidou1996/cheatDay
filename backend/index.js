1a3dfa1db72022a54f4527fcf86d83b299885d2c
Andrew Evripidou <aevripidou1996@gmail.com>
2023-07-23 17:26:14 +1000

    finished authentication

--- backend/index.js
+++ backend/index.js
@@ -8,5 +8,10 @@ const app = express()
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGO_URL, () => console.log('DB is successfully connected'))
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/auth', authController)
+
+
 // start server
 app.listen(process.env.PORT, () => console.log('server has been started successfully'))
