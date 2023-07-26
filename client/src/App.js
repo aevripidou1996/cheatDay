bfb9a005d198b9c91fe68254b990af22dae28e7d
Andrew Evripidou <aevripidou1996@gmail.com>
2023-07-26 23:08:10 +1000

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
-import Navbar from './components/navbar/Navbar';
 import Navbar from 'src/components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
 import Signup from './components/signup/Signup';
