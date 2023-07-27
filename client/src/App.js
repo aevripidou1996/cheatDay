3b73f9c846345fc832d17722c686b3f8843df469
Andrew Evripidou <aevripidou1996@gmail.com>
2023-07-27 11:33:22 +1000

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Navbar from '../src/components/nav/Navbar';
-import Footer from './src/components/footer/Footer';
-import Home from './src/components/home/Home';
-import Login from './src/components/login/Login';
-import Signup from './src/components/signup/Signup';
-import Create from './src/components/create/Create';
-import FoodDetails from './src/components/foodDetails/FoodDetails';
-import FoodCatalog from './src/components/foodCatalog/FoodCatalog';
-import Cart from './src/components/cart/Cart';
-import Checkout from './src/components/checkout/Checkout';
+import Footer from '../src/components/footer/Footer'
+import Home from '../src/components/home/Home';
+import Login from '../src/components/login/Login';
+import Signup from '../src/components/signup/Signup';
+import Create from '../src/components/create/Create';
+import FoodDetails from '../src/components/foodDetails/FoodDetails';
+import FoodCatalog from '../src/components/foodCatalog/FoodCatalog';
+import Cart from '../src/components/cart/Cart';
+import Checkout from '../src/components/checkout/Checkout';
 import {useLocation} from 'react-router-dom'
 import { useEffect } from 'react';
 
